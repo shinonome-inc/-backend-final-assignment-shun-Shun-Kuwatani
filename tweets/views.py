c2f2db541cbab489ff770e7390a1e4292400c57f
Kuwatani0526 <s.kuwatani@4nonome.com>
2023-03-12 17:01:22 +0900

    最終課題サインアップページ

--- tweets/views.py
+++ tweets/views.py
@@ -1,1 +1,7 @@
-# from django.shortcuts import render
+from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+class HomeView(LoginRequiredMixin, TemplateView):
+    template_name = "tweets/home.html"
